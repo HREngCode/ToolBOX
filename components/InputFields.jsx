e5b786140511ee5062f4874aace403dd96a1d330
HREngCode <stallionranchmedia@gmail.com>
2025-07-23 18:30:30 -0500

    Add homepage formatting

---
+++ components/InputFields.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+//Component Imports
+
+//Context Imports
+
+// CSS Imports
+// import "/src/NavBar.css";
+
+function InputFields() {
+  return (
+    <>
+      <div className="center-container">
+        {/* <!-- <div className="center-heading">Text Goes Here</div> --> */}
+        <div className="input-container">
+          <div>
+            <button>Sign Up</button>
+          </div>
+          <div>
+            <button>Log In</button>
+          </div>
+          <div>
+            Welcome to the ToolBox where you can can take an inventory of our
+            posessions and keep them organized in the cloud. Whether it's your
+            wardrobe or your dishes, you can have an easy way to count your
+            belongings. We are here to meet your needs. Don't see a category
+            that applies? Let us know! We are always working to make improvemnts
+            and we would love to hear from you!!
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default InputFields;
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-import ShedImage from "./assets/shed.jpg";
 import NavBar from "../components/NavBar.jsx";
+import InputFields from "../components/InputFields.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,33 +12,12 @@ function App() {
         <div>
           <NavBar />
         </div>
-        <div className="center-container">
-          <div className="center-heading">Text Goes Here</div>
-          <div>
-            <div className="input-container">
-              <div>
-                <button>Sign Up</button>
-                <button>Log In</button>
-              </div>
-              <div>
-                <label htmlFor="label">First Name</label>
-                <input type="text" />
-              </div>
-              <div>
-                <label htmlFor="label">Last Name</label>
-                <input type="text" />
-              </div>
-              <div>
-                Welcome to the ToolBox where you can can take an inventory of
-                our posessions and keep them organized in the cloud. Whether
-                it's your wardrobe or your dishes, you can have an easy way to
-                count your belongings. We are here to meet your needs. Don't see
-                a category that applies? Let us know! We are always working to
-                make improvemnts and we would love to hear from you!!
-              </div>
-            </div>
-            <img />
-          </div>
+        <div class="homepage-grid">
+          <img src="./src/assets/Screwdrivers.jpg" alt="Screwdrivers"></img>
+          <img src="./src/assets/wrenches.jpg" alt="Wrenches"></img>
+        </div>
+        <div>
+          <InputFields />
         </div>
       </div>
     </>
